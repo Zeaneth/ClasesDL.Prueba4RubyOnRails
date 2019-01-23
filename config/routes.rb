b748df991c05e6bd36ad2631942ced63acf029fe
Zamiz <zamiztobar@gmail.com>
2019-01-23 02:01:37 -0300

    controller: add tasks controller with index routes: add 'resources: tasks' for index routes: set tasks index as root path db: add tasks seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :tasks, only: :index
+  
+  root to: 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+tasks = Task.create([
+    {
+      name: 'Comer choripán',
+      photo: 'https://ethnicfoodsrus.com/wp-content/uploads/2015/05/Choripan-con-Chimichurri-Public-Domain.jpg',
+      done: true
+    },
+    {
+      name: 'Comer anticucho',
+      photo: 'http://www.saborcitoperuano.com/recetas/wp-content/uploads/2016/04/anticuchos_saborcitoperuano_recetas-800x600.jpg',
+      done: false
+    },
+    {
+      name: 'Hacer asado',
+      photo: 'http://asadacho.com/wp-content/uploads/2015/01/asado-parrilla.jpg',
+      done: false
+    },
+    {
+      name: 'Tomar chicha',
+      photo: 'https://t2.kn3.net/taringa/5/2/8/3/6/3/QUIROOXZ/0ED.jpg',
+      done: false
+    },
+    {
+      name: 'Tomar terremoto',
+      photo: 'https://chocale.cl/wp-content/uploads/2018/09/terremoto-trago-chileno.jpg',
+      done: false
+    },
+    {
+      name: 'Bailar cueca',
+      photo: 'https://blog.airkeep.me/wp-content/uploads/2018/09/blog_airkeep_cueca_photo_by_radiozero.cl_.jpg',
+      done: false
+    },
+    {
+      name: 'Jugar rayuela',
+      photo: 'http://www.diarioelcentro.cl/imagenes/noticias/publico-disfruto-de-la-rayuela-en-fiesta-de-la-chilenidad.jpg',
+      done: false
+    },
+    {
+      name: 'Comer empanadas de pino',
+      photo: 'https://blogs.hope.edu/students/wp-content/uploads/sites/6/2016/10/Empanada-de-Pino.png',
+      done: false
+    },
+    {
+      name: 'Disfrutar con los amigues',
+      photo: 'http://www.utalca.cl/medios/utalca/prensa/galerias/galeria_11.jpg',
+      done: false
+    }
+])
