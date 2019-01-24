84aaab8dd260fa08d4d527f973cbe661eee78661
Zamiz <zamiztobar@gmail.com>
2019-01-24 16:39:07 -0300

    db: create join table and 'n to n' relation in user and task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_23_041425) do
+ActiveRecord::Schema.define(version: 2019_01_24_192849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2019_01_23_041425) do
     t.boolean "done", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "task_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
